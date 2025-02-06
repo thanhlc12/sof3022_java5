2baddac159e4e3fa1b5621b8689013743f4e8436
ThanhLC127 <thanhlcph51863@gmail.com>
2025-02-06 22:06:40 +0700

    update

--- src/main/java/com/example/demo/controller/HocSinhController.java
+++ src/main/java/com/example/demo/controller/HocSinhController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -22,5 +24,24 @@ public class HocSinhController {
             System.out.println(hocSinh.toString());
         }
         return "/home.html";
+    }
+
+    @PostMapping("/hoc-sinh/add")
+    public String add(HocSinh hocSinh) {
+        hocSinhRepo.save(hocSinh);
+        return "redirect:/hoc-sinh/hien-thi";
+    }
+
+    @GetMapping("/hoc-sinh/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+        hocSinhRepo.deleteById(id);
+        return "redirect:/hoc-sinh/hien-thi";
+    }
+
+    @GetMapping("/hoc-sinh/detail/{id}")
+    public String detail(@PathVariable("id") Integer id, Model model) {
+        HocSinh hocSinh = hocSinhRepo.findById(id).get();
+        model.addAttribute("hocSinh", hocSinh);
+        return "/detail.html";
     }
 }
